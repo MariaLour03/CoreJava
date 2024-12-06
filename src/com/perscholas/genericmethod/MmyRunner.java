2928674a282388f3ffec003b537974994ad3f91b
Maria Lourdusamy <maria.lourdusamy@gmail.com>
2024-12-06 14:26:14 -0600

    Commit GLAB-303.11.1 - Generic Method and Class

---
+++ src/com/perscholas/genericmethod/MmyRunner.java
@@ -0,0 +1,22 @@
+package com.perscholas.genericmethod;
+
+public class MmyRunner {
+    public static void main(String[] args) {
+        // initialize generic class
+        // with String and Integer data
+
+        GMultipleDatatype<String, Integer> mobj = new GMultipleDatatype("Per Scholas", 11025);
+
+        System.out.println(mobj.getValueOne());
+        System.out.println(mobj.getValueTwo());
+
+        // initialize generic class
+        // with String and String data
+        GMultipleDatatype<String, String> mobj2 = new GMultipleDatatype("Per Scholas", "Non profit");
+
+        System.out.println(mobj2.getValueOne());
+        System.out.println(mobj2.getValueTwo());
+    }
+}
+
+
